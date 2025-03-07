91cf31a00a87ab7f98d6032001ad54ed064a4f84
vasapolin <victor.vencedor2005@gmail.com>
2025-03-07 13:50:11 +0000

    Adiciona Scripts ´link:check´ e ´links:fix´

--- index.js
+++
@@ -1,5 +0,0 @@
-funtion Home () {
-  return <h1>teste</h1>
-}
-
-export default home
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>te amo laura</h1>
-}    
+  return <h1>te amo laura, meu amor</h1>;
+}
 
 export default Home;
